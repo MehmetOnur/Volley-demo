9cc7302b8cfcf1a29f378f6189704116a819c5e8
Onur Uzuner <onur.uzuner@kobil.com>
2017-01-02 15:31:56 +0300

    volley json req.

---
+++ app/src/main/java/com/github/mehmetonur/volley_demo/utils/Const.java
@@ -0,0 +1,17 @@
+package com.github.mehmetonur.volley_demo.utils;
+
+/**
+ * Created by Mehmet Onur UZUNER on 02/01/2017.
+ * Copyright © 2017 KOBIL Systems GmbH. All rights reserved.
+ */
+
+public class Const {
+  public static final String URL_JSON_OBJECT =
+      "http://api.androidhive.info/volley/person_object.json";
+  public static final String URL_JSON_ARRAY =
+      "http://api.androidhive.info/volley/person_array.json";
+  public static final String URL_STRING_REQ =
+      "http://api.androidhive.info/volley/string_response.html";
+  public static final String URL_IMAGE = "http://api.androidhive.info/volley/volley-image.jpg";
+}
+
